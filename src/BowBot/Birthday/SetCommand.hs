a51734d965c5719b728fdcc2d9042f59f395595a
Greg C <gregory1234@users.noreply.github.com>
2023-06-10 22:43:10 +0200

    Reimplemented ?bdset

--- src/BowBot/Birthday/SetCommand.hs
+++ src/BowBot/Birthday/SetCommand.hs
@@ -1,9 +1,9 @@
 module BowBot.Birthday.SetCommand where
 
 import BowBot.Command
-import BowBot.Discord.Account
 import BowBot.Discord.Utils
 import BowBot.Birthday.Basic
+import BowBot.Command.Utils
   
 setBirthdayCommand :: Command
 setBirthdayCommand = Command CommandInfo
@@ -11,5 +11,8 @@ setBirthdayCommand = Command CommandInfo
   , commandHelpEntries = [HelpEntry { helpUsage = "bdset [discord/discord id] [day(1-31).month(1-12)]", helpDescription = "override someone's birthday", helpGroup = "normal" }]
   , commandPerms = ModLevel
   , commandTimeout = 15
-  } $ twoArguments $ \didStr dbStr -> do
-    undefined
+  } $ twoArguments $ \didStr bdStr -> do
+    did <- liftMaybe theDiscordIdIsInvalid $ discordIdFromString didStr
+    bd <- liftMaybe "*Invalid birthday date!*" $ birthdayFromString bdStr
+    a <- setBirthday did bd
+    respond $ if a then "*Birthday set!*" else somethingWentWrongMessage
