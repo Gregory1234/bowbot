836b353fb088444e8c304f900e15ac6e9b9d8acf
Greg C <gregory1234@users.noreply.github.com>
2023-06-10 22:55:08 +0200

    Reimplemented ?selectmc

--- src/BowBot/Minecraft/SelectCommand.hs
+++ src/BowBot/Minecraft/SelectCommand.hs
@@ -7,7 +7,8 @@ import BowBot.Discord.Utils
 import BowBot.BotData.Cached
 import BowBot.DB.Basic
 import BowBot.Minecraft.Basic (uuidString)
-
+import BowBot.Command.Utils
+import Control.Monad.Except
 
 selectMinecraftCommand :: Command
 selectMinecraftCommand = Command CommandInfo
@@ -16,4 +17,14 @@ selectMinecraftCommand = Command CommandInfo
   , commandPerms = DefaultLevel
   , commandTimeout = 30
   } $ oneArgument $ \str -> do
-    undefined
+    did <- userId <$> envs envSender
+    bbacc <- liftMaybe youArentRegisteredMessage =<< getBowBotAccountByDiscord did
+    mc <- liftMaybe thePlayerDoesNotExistMessage =<< getMinecraftAccountByCurrentNameFromCache str
+    when (mcUUID mc `notElem` accountMinecrafts bbacc) $ throwError "*This account doesn't belong to you! If you own this account, ask an admin to add it for you.*"
+    when (mcUUID mc == accountSelectedMinecraft bbacc) $ throwError "*This account is selected already!*"
+    a <- (>0) <$> executeLog "INSERT INTO `peopleMinecraft` (`minecraft`, `selected`) VALUES (?,0),(?,1) ON DUPLICATE KEY UPDATE `selected`=VALUES(`selected`)" (uuidString $ accountSelectedMinecraft bbacc, uuidString $ mcUUID mc)
+    if a then do
+      cache <- getCache
+      liftIO $ atomically $ modifyTVar cache (insertMany [(accountBotId bbacc, bbacc { accountSelectedMinecraft = mcUUID mc})])
+      respond "*Selected account updated!*"
+    else respond somethingWentWrongMessage
