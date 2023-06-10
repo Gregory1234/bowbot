a839b39a640186529a8b9904b6128514c2b3c355
Greg C <gregory1234@users.noreply.github.com>
2023-06-10 22:45:21 +0200

    Reimplemented ?sban

--- src/BowBot/Hypixel/BanCommand.hs
+++ src/BowBot/Hypixel/BanCommand.hs
@@ -5,6 +5,7 @@ import BowBot.Minecraft.Account
 import BowBot.Discord.Utils
 import BowBot.Hypixel.Leaderboard
 import BowBot.Hypixel.LeaderboardStatus
+import BowBot.Command.Utils
   
 hypixelBanCommand :: Command
 hypixelBanCommand = Command CommandInfo
@@ -12,5 +13,12 @@ hypixelBanCommand = Command CommandInfo
   , commandHelpEntries = [HelpEntry { helpUsage = "sban [name]", helpDescription = "ban a player from the leaderboard", helpGroup = "normal" }]
   , commandPerms = ModLevel
   , commandTimeout = 15
-  } $ oneArgument $ \str -> do
-    undefined
+  } $ oneArgument $ \name -> do
+    mc <- liftMaybe thePlayerDoesNotExistMessage =<< getMinecraftAccountByCurrentNameFromCache name
+    banned <- getHypixelIsBannedByUUID (mcUUID mc)
+    if banned == Banned
+      then respond "*The player is already banned!*"
+      else do
+        a <- setHypixelIsBannedByUUID (mcUUID mc) Banned
+        b <- removeHypixelBowLeaderboardEntryByUUID (mcUUID mc)
+        respond $ if a && b then "*Success, player got banned!*" else somethingWentWrongMessage
